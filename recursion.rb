42b2596e5d4795897ba3c501a4872f3b29021c74
carveyh <carvey.hor@gmail.com>
2023-03-22 15:43:10 -0400

    Added #bsearch

--- recursion.rb
+++ recursion.rb
@@ -92,9 +92,36 @@ def iter_fib(n)
 
 end
 
-p iter_fib(1)
-p iter_fib(2)
-p iter_fib(3)
-p iter_fib(6)
-p iter_fib(10)
+# p iter_fib(1)
+# p iter_fib(2)
+# p iter_fib(3)
+# p iter_fib(6)
+# p iter_fib(10)
 
+#Possible refactor with <=> ?
+def bsearch(arr, target)
+    return nil if arr.empty?
+    mid_idx = arr.length / 2
+    return mid_idx if arr[mid_idx] == target
+    left = arr[0...mid_idx]
+    right = arr[(mid_idx + 1)..-1]
+    if target < arr[mid_idx]
+        bsearch(left, target)
+    # elsif target > arr[mid_idx] && !right.empty?
+    #     mid_idx + 1 + bsearch(right, target)
+    # else
+    #     nil
+    else
+        right_search = bsearch(right, target)
+        return nil if right_search == nil
+        mid_idx + 1 + right_search
+    end
+end
+
+p bsearch([1, 2, 3], 1) # => 0
+p bsearch([2, 3, 4, 5], 3) # => 1
+p bsearch([2, 4, 6, 8, 10], 6) # => 2
+p bsearch([1, 3, 4, 5, 9], 5) # => 3
+p bsearch([1, 2, 3, 4, 5, 6], 6) # => 5
+p bsearch([1, 2, 3, 4, 5, 6], 0) # => nil
+p bsearch([1, 2, 3, 4, 5, 7], 6) # => nil
